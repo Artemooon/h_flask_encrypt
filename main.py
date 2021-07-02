93776aa81ef98564c5653d47407e65c1fb229f2b
Artem <artem.logachov773@gmail.com>
2021-07-02 20:25:42 +0300

    final commit

--- main.py
+++ main.py
@@ -6,21 +6,24 @@ app = Flask(__name__)
 key = Fernet.generate_key()
 fernet_key = Fernet(key)
 
-
+#Route encrypt returns encrypted string
 @app.route("/encrypt")
 def encrypt():
     string = request.args.get('string','Test')
+    #Encrypting our string
     token = f.encrypt(bytes(string, 'utf-8'))
 
     is_encrypt = True
     return render_template('index.html', encrypted_string = str(token), is_encrypt = is_encrypt)
 
+#Route decrypt returns decrypted string
 @app.route("/decrypt")
 def decrypt():
     try:
         string = request.args.get('string',None)
         if not string:
             return 'Invalid'
+        #Decrypting our string    
         token = f.decrypt(bytes(string, 'utf-8'))
         is_encrypt = False
         return render_template('index.html', decrypted_string = str(token),is_encrypt = is_encrypt)
